36baf0b6690cc11b5409a0488007abd76dab4172
Pratiksha Pawar <pratikshapawar920@gmail.com>
2025-01-09 13:52:25 +0530

    Project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ProjectYummy from '../Images/ProjectYummy.png'
+import ProjectPhotoApp from '../Images/ProjectPhotoApp.png'
+import ProjectOsumare from '../Images/ProjectOsumare.png'
 const Projects = () => {
   return (
     <section id="projects" className="projects-section  my-5">
@@ -25,37 +27,36 @@ const Projects = () => {
           
               <div className="project-card">
                 <img 
-                  src="https://via.placeholder.com/300x200" 
+                  src={ProjectOsumare} 
                   alt="Project 2" 
                   className="project-image"
                 />
                 <div className="project-info">
                   <h3 className="project-title">Osumre Project</h3>
                   <p className="project-description">
-                    
+                  Created a professional landing page to showcase  work and services.The frontend, developed using HTML, CSS, Bootstrap, React.js, and JavaScript, ensures a visually appealing, responsive design. This project demonstrates my skills in full-stack development, with a strong focus on UI/UX design and backend functionalit
                   </p>
                   <a href="https://github.com/pratikshapawar0906/Assignment-c1" className="project-link">View on GitHub</a>
                 </div>
               </div>
           
-              {/* <div className="project-card">
+              <div className="project-card">
                 <img 
-                  src="https://via.placeholder.com/300x200" 
+                  src={ProjectPhotoApp} 
                   alt="Project 3" 
                   className="project-image"
                 />
                 <div className="project-info">
                   <h3 className="project-title">Project 3</h3>
                   <p className="project-description">
-                    A weather forecasting app built with JavaScript and APIs that provides real-time weather data based on user location.
+                  Developed a dynamic web application to showcase a photographer's portfolio, demonstrating their skills and photos. The backend was built using MongoDB, Node.js, and Express.js, ensuring secure storage and management of photo data. The frontend was designed with HTML, CSS, Bootstrap, and React.js, providing a responsive and user-friendly interface. This project highlights full-stack development capabilities with a focus on seamless UI/UX and efficient backend performance.
                   </p>
-                  <a href="https://github.com/your-username/project3" className="project-link">View on GitHub</a>
-                  https://myportfoliopratiksha.netlify.app/
+                  <a href="https://github.com/pratikshapawar0906/PhotoApp" className="project-link">View on GitHub</a>
                 </div>
-              </div> */}
+              </div>
             </div>
           </section>
   );
 };
-
+ //https://myportfoliopratiksha.netlify.app/
 export default Projects;
